252a9da0a670c0160a10fa5d34854274940b6d55
Moussa <moussaabdallahhoumadi@gmail.com>
2025-08-04 14:12:56 +0200

    refactor: remove requirement system

--- src/main/java/fr/moussax/blightedMC/core/items/requirements/Requirement.java
+++
@@ -1,9 +0,0 @@
-package fr.moussax.blightedMC.core.items.requirements;
-
-import fr.moussax.blightedMC.core.players.BlightedPlayer;
-import org.bukkit.inventory.ItemStack;
-
-public interface Requirement {
-  boolean isAuthorized(BlightedPlayer player, ItemStack itemStack);
-  default String requirementNotificationMessage() { return ""; }
-}
