5a2bf8e9acee5bb28abf7901d5c8827cdff9a78a
Moussa <moussaabdallahhoumadi@gmail.com>
2025-08-05 17:43:42 +0200

    refactor(menus): remove old logic

--- src/main/java/fr/moussax/blightedMC/core/menus/BlightedInventory.java
+++
@@ -1,7 +0,0 @@
-package fr.moussax.blightedMC.core.menus;
-
-import org.bukkit.inventory.InventoryHolder;
-
-public interface BlightedInventory extends InventoryHolder {
-  ClickableItem getItemAt(int slot);
-}
