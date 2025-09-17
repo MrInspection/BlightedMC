aa78d9c85177fd002e2cf9ce0f93577d16582d57
Moussa <moussaabdallahhoumadi@gmail.com>
2025-09-17 19:55:53 +0200

    feat(utils): add sound utility classes

---
+++ src/main/java/fr/moussax/blightedMC/utils/sound/BlightedSound.java
@@ -0,0 +1,13 @@
+package fr.moussax.blightedMC.utils.sound;
+
+import fr.moussax.blightedMC.utils.Utilities;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+
+import java.util.Objects;
+
+public record BlightedSound(Sound sound, float volume, float pitch, long delay) {
+  public void play(Location location) {
+    Utilities.delay(() -> Objects.requireNonNull(location.getWorld()).playSound(location, sound, volume, pitch), delay);
+  }
+}
