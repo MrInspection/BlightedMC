900ce7185a7f4f9aa1b2d3a443ac9142bc71b14d
Moussa <moussaabdallahhoumadi@gmail.com>
2025-12-26 20:15:54 +0100

    refactor: remove files

--- src/main/java/fr/moussax/blightedMC/core/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Core package for the BlightedMC plugin.
- *
- * <p>Contains the central classes and utilities that manage
- * core functionality, game mechanics, and plugin-wide services.</p>
- */
-package fr.moussax.blightedMC.core;
--- src/main/java/fr/moussax/blightedMC/game/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Game package for the BlightedMC plugin.
- *
- * <p>Contains concrete implementations of core elements,
- * including game mechanics, entities, items, and behaviors
- * built upon the abstractions defined in {@link fr.moussax.blightedMC.core}.</p>
- */
-package fr.moussax.blightedMC.game;
