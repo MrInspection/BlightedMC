7c910f97ed393dd6c02e3d80212acd4aa3672d77
Moussa <moussaabdallahhoumadi@gmail.com>
2026-07-04 13:05:56 +0200

    feat(player): grant player the riddle and banner upon spawning

--- src/main/java/fr/moussax/blightedMC/engine/player/PlayerListener.java
+++ src/main/java/fr/moussax/blightedMC/engine/player/PlayerListener.java
@@ -2,7 +2,9 @@ package fr.moussax.blightedMC.engine.player;
 
 import fr.moussax.blightedMC.engine.entities.BlightedEntity;
 import fr.moussax.blightedMC.engine.entities.listeners.BlightedEntitiesListener;
+import fr.moussax.blightedMC.engine.items.registry.ItemRegistry;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +12,8 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.*;
 
@@ -20,6 +24,30 @@ public final class PlayerListener implements Listener {
         Player player = event.getPlayer();
         new BlightedPlayer(event.getPlayer());
         event.setJoinMessage("§8 ■ §f" + event.getPlayer().getName() + " §7joined the SMP.");
+
+        if (!player.hasPlayedBefore()) {
+            player.getInventory().setHelmet(ItemRegistry.getItem("BLIGHTED_BANNER").toItemStack());
+            player.getInventory().setItemInOffHand(ItemRegistry.getItem("BLIGHTED_CODEX").toItemStack());
+
+            player.setHealth(4.0);
+            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100, 0, false, false, false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 140, 0, false, false, false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, 80, 1, false, false, false));
+
+            player.sendTitle("§f§lThe Blight Awakens", "§7The whispers fade into reality...", 10, 80, 20);
+
+            player.sendMessage("\n \n");
+            player.sendMessage(" §f" + player.getName() + "§7, the §5§lBlight §7has already begun to take root...\n ");
+            player.sendMessage(" §8§oYou claw your way back to consciousness.");
+            player.sendMessage(" §cYour vitality is drained§8§o, your vision clouded...");
+            player.sendMessage(" §8§o...and a §5chilling tome §8§ois bound to your left hand.");
+            player.sendMessage("\n \n");
+
+            player.playSound(player.getLocation(), Sound.ENTITY_ELDER_GUARDIAN_CURSE, 0.4f, 0.5f);
+            player.playSound(player.getLocation(), Sound.ENTITY_WARDEN_HEARTBEAT, 1.0f, 0.5f);
+            player.playSound(player.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_DEPLETE, 1.0f, 0.5f);
+            player.playSound(player.getLocation(), Sound.AMBIENT_CRIMSON_FOREST_MOOD, 1.5f, 0.7f);
+        }
     }
 
     @EventHandler
