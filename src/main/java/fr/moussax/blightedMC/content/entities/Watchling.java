e60c5cda44d66be4aa47b33765d03e9ee7d2ec25
Moussa <moussaabdallahhoumadi@gmail.com>
2026-08-23 15:26:04 +0200

    refactor(entities): replace watchling logic with attachment system, streamline damage handling, and add BlightswornHusk hunger effect

--- src/main/java/fr/moussax/blightedMC/content/entities/Watchling.java
+++ src/main/java/fr/moussax/blightedMC/content/entities/Watchling.java
@@ -4,7 +4,6 @@ import fr.moussax.blightedMC.content.utils.ai.EndermanAI;
 import fr.moussax.blightedMC.engine.entities.EntityLootTableBuilder;
 import fr.moussax.blightedMC.engine.entities.spawnable.SpawnableEntity;
 import fr.moussax.blightedMC.engine.entities.spawnable.condition.SpawnRules;
-import fr.moussax.blightedMC.utils.Utilities;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -83,7 +82,7 @@ public class Watchling extends SpawnableEntity {
         entity.swingMainHand();
         entity.swingOffHand();
 
-        Utilities.delay(() -> {
+        addCoreDelayedAction(10L, () -> {
             if (!isAlive() || target.getLocation().distance(entity.getLocation()) > 3) return;
 
             target.damage(getDamage() * 2, entity);
@@ -92,7 +91,7 @@ public class Watchling extends SpawnableEntity {
             if (random.nextBoolean()) {
                 executeEscapeTeleport();
             }
-        }, 10);
+        });
     }
 
     private void executeEscapeTeleport() {
@@ -103,9 +102,9 @@ public class Watchling extends SpawnableEntity {
         );
         entity.teleport(escapeLoc);
         entity.setInvisible(true);
-        Utilities.delay(() -> {
+        addCoreDelayedAction(40L, () -> {
             if (isAlive()) entity.setInvisible(false);
-        }, 40);
+        });
     }
 
     @Override
--- src/main/java/fr/moussax/blightedMC/content/entities/factions/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides faction-specific blighted entity implementations and archetype definitions.
- */
-package fr.moussax.blightedMC.content.entities.factions;
